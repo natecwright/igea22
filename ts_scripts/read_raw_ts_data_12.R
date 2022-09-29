5590896b04a5bd25505bfe6c2377dba2a109f822
naterd0g <nathanielwri@umass.edu>
2022-09-29 15:54:38 -0400

    nate's work

---
+++ ts_scripts/read_raw_ts_data_12.R
@@ -0,0 +1,53 @@
+# script to read in basic data and look at it
+library(dplyr)
+library(readxl)
+
+setwd('C:/Users/ncw02/Downloads/IGEA/')
+
+# read in data ----------
+# setwd (path to the data)
+
+ts1_excel = read_xlsx('raw_ts_data/fd/fd12_ts1.xlsx')
+
+
+ts2_excel = read_xlsx('raw_ts_data/fd/fd12_ts2.xlsx')
+
+metadata_excel = read_xlsx('raw_ts_data/fd/fd12_metadata.xlsx')
+
+# extract reach from txt file for ts1 (should be identical for ts2)
+ts1_reach = read.delim('raw_ts_data/group12/ep7_ts1.txt',
+                       skip = 13, header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
+  transmute(Reach = strsplit(V1, " +")[[1]][3])
+
+# extract reach from txt file for ts2 (should be identical for ts1)
+ts2_reach = read.delim('raw_ts_data/group12/ep7_ts2.txt',
+                        skip = 14, header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
+  transmute(Reach = strsplit(V1, " +")[[1]][3])
+
+# read in ts1 xyz
+ts1_txt = read.delim("raw_ts_data/group12/ep7_ts1.txt",
+                      skip = 19, header = TRUE, nrows= 115, dec = ".", sep = ',')%>%
+  mutate(TS_code = "TS1")#%>%
+  #(Reach = ts1_txt$Reach)
+
+# read in ts2 xyz
+ts2_txt = read.delim("raw_ts_data/group12/ep7_ts2.txt",
+                      skip = 20, header = TRUE, nrows= 124, dec = ".", sep = ',')%>%
+  mutate(TS_code = "TS2")#%>%
+  #mutate(Reach = ts2_txt$Reach)
+
+
+# -------------------
+
+
+# test a join ----
+joined_df = left_join(ts1_excel, metadata_in, by='Reach')%>%
+  filter(Reach == 'E7')%>%
+  select('Reach', 'Point ID', 'Elevation')
+
+combined_df = rbind(ts1_txt, ts2_txt)
+
+#plot(joined_df$'Point ID', joined_df$Elevation)
+
+# -------
+
