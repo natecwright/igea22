b449b308cc223f0a0e2ce0ef29efceb5ca780971
naterd0g <nathanielwri@umass.edu>
2022-09-30 11:12:27 -0400

    nate and stella work 9/30

--- ts_scripts/read_raw_ts_data_12.R
+++ ts_scripts/read_raw_ts_data_12.R
@@ -9,7 +9,6 @@ setwd('C:/Users/ncw02/Downloads/IGEA/')
 
 ts1_excel = read_xlsx('raw_ts_data/fd/fd12_ts1.xlsx')
 
-
 ts2_excel = read_xlsx('raw_ts_data/fd/fd12_ts2.xlsx')
 
 metadata_excel = read_xlsx('raw_ts_data/fd/fd12_metadata.xlsx')
@@ -27,25 +26,37 @@ ts2_reach = read.delim('raw_ts_data/group12/ep7_ts2.txt',
 # read in ts1 xyz
 ts1_txt = read.delim("raw_ts_data/group12/ep7_ts1.txt",
                       skip = 19, header = TRUE, nrows= 115, dec = ".", sep = ',')%>%
-  mutate(TS_code = "TS1")#%>%
-  #(Reach = ts1_txt$Reach)
+  mutate(TS_code = "TS1")%>%
+  mutate(Reach = ts1_reach$Reach)
 
 # read in ts2 xyz
 ts2_txt = read.delim("raw_ts_data/group12/ep7_ts2.txt",
                       skip = 20, header = TRUE, nrows= 124, dec = ".", sep = ',')%>%
-  mutate(TS_code = "TS2")#%>%
-  #mutate(Reach = ts2_txt$Reach)
+  mutate(TS_code = "TS2")%>%
+  mutate(Reach = ts2_reach$Reach)
 
 
 # -------------------
 
 
 # test a join ----
-joined_df = left_join(ts1_excel, metadata_in, by='Reach')%>%
-  filter(Reach == 'E7')%>%
-  select('Reach', 'Point ID', 'Elevation')
 
-combined_df = rbind(ts1_txt, ts2_txt)
+joined_df1 = left_join(ts1_excel, metadata_excel, by='Reach')%>%
+  filter(Reach == 'E7')#%>%
+  #select('Reach', 'Point ID', 'Elevation')
+
+joined_df2 = left_join(ts2_excel, metadata_excel, by='Reach')%>%
+  filter(Reach == 'E7')#%>%
+  #select('Reach', 'Point ID', 'Elevation')
+
+joined_df3 = left_join(joined_df1, ts1_txt, by='Reach')%>%
+  filter(Reach == 'E7')#%>%
+#select('Reach', 'Point ID', 'Elevation')
+
+
+#combined_df = rbind(ts1_txt, ts2_txt)
+
+combined_df = rbind(joined_df1, joined_df2)
 
 #plot(joined_df$'Point ID', joined_df$Elevation)
 
