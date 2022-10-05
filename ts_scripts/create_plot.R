b03c5f0afff5ef9e722b022abd536293c11423b3
StellaWenczel <wenczel@umass.edu>
2022-10-05 19:33:31 -0400

    Update create_plot.R

--- ts_scripts/create_plot.R
+++ ts_scripts/create_plot.R
@@ -12,12 +12,13 @@ ep7=readRDS('outputs/ep7.rds')#%>%
 #filter(Cross.section==1)
 
 p1=ggplot(ep7)+
-  geom_point(aes(y=North,x=East, colour=as.factor(Cross.section)))+
-  theme_bw()+
+  geom_point(aes(y=North,x=East, colour=as.factor(Cross.section)))+ #as.factor makes discreet colors for the points
+  theme_bw()+ #gets rid of grey background
   xlab('Easting (m)')+
-  ylab('Northing')+
+  ylab('Northing (m)')+
+  labs(colour = 'Cross Section')+ #title of the legend
   theme(legend.text=element_text(size=14),
-        legend.title=element_blank(),
+        legend.title=element_text(size=14),
         legend.position ='left',
         axis.text= element_text(size=14),
         axis.title= element_text(size=14),
