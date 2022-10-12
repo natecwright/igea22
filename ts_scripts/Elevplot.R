c81caf704657951536e9ca7d08114a9d37d18698
StellaWenczel <wenczel@umass.edu>
2022-10-12 16:04:53 -0400

    Update Elevplot.R

--- ts_scripts/Elevplot.R
+++ ts_scripts/Elevplot.R
@@ -9,7 +9,7 @@ setwd('/Users/Stella/OneDrive - University of Massachusetts/Documents/IGEA/Munge
 
 #read in file
 ep7_elev=readRDS('outputs/ALT.rds')#%>%
-#filter(Cross.section==1)
+filter(UID2==E7W)
 
 p2=ggplot(ep7_elev)+
   geom_point(aes(y=ElevationA, x=UID2, colour="blue"))+ #as.factor makes discreet colors for the points
