c7a8810fd4d3e3c6f673fa30cb8d204db4b8e221
etboudreau <etboudreau@umass.edu>
2022-10-19 13:26:48 -0400

    editing 10/19

--- ts_scripts/read_raw_ts_data_3.R
+++ ts_scripts/read_raw_ts_data_3.R
@@ -2,8 +2,7 @@
 library(dplyr)
 library(readxl)
 library(tidyr)
-#install.packages("stringr")
-library("stringr")
+library(stringr)
 
 setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
 
@@ -25,37 +24,43 @@ metadata_excel = read_xlsx('raw_ts_data/fd/fd3_metadata.xlsx')
 #read in reach code
 ts1_reach = read.delim("raw_ts_data/group3.n/ep7_ts1.txt",
                        header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
-  transmute(Reach = strsplit(V1, " +")[[1]][3])
-
+  transmute(Reach = V1)
 
 ts2_reach = read.delim("raw_ts_data/group3.n/ep7_ts2.txt",
                          header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
-  transmute(Reach = strsplit(V1, " +")[[1]][3])
+  transmute(Reach = V1)
 
 ts1_txt = read.delim("raw_ts_data/group3.n/ep7_ts1.txt",
                       skip = 1, header = TRUE, dec = ".", sep = ',')%>%
   mutate(TS_code = "1")%>%
-  mutate(Reach = ts1_reach$Reach)
+  mutate(Reach = ts1_reach$Reach)%>%
+  mutate(PointID=as.double(PointID))
 
   
 ts2_txt = read.delim("raw_ts_data/group3.n/ep7_ts2.txt",
                       skip = 1, header = TRUE, dec = ".", sep = ',')%>%
   mutate(TS_code = "2")%>%
-  mutate(Reach = ts2_reach$Reach)
+  mutate(Reach = ts2_reach$Reach)%>%
+  mutate(PointID=as.double(PointID))
+#----
 
 ts_excel_df = rbind(ts1_excel,ts2_excel)
 
 ts_txt_df = rbind(ts1_txt,ts2_txt)
 
+  #select(ts_txt_df,-Code) #trying to delete column code
+
+
 #created data frame with excel and text file for specific reach
 joined_excel_txt_df = left_join(ts_excel_df, ts_txt_df, by=c('Reach', 'TS_code', 'PointID')) %>%
   mutate(uniqueID = paste(Reach,TS_code,Location,XSection)) %>%
-  mutate(AP = substr(uniqueID,9,9)) %>%
-    pivot_wider(names_from = AP, values_from = Elevation) %>%
-  mutate(ID = str_replace_all(uniqueID,"A","")) %>%
-  mutate(ID = str_replace_all(ID,"P",""))%>%
-  group_by(ID)%>%
-  mutate(delta_elev = sum(A,na.rm=T)-sum(P,na.rm=T))
+  mutate(AP = substr(uniqueID,9,9)) 
+  #mutate(LWR=substr(uniqueID,))
+    #pivot_wider(names_from = AP, values_from = Elevation) %>%
+  #mutate(ID = str_replace_all(uniqueID,"A","")) %>%
+  #mutate(ID = str_replace_all(ID,"P",""))%>%
+  #group_by(ID)%>%
+  #mutate(delta_elev = sum(A,na.rm=T)-sum(P,na.rm=T))
 #sum is used because there are two A (and P) values for each ID one is an elevation and the other is an NA
 #,na.rm=T tells the script to ignore the NA and continue with calculation
   
