7754bdd42c23770bbffd01adde48ee51cd2464b7
oskaroh <oohara@umass.edu>
2022-11-02 19:28:59 -0400

    added comments to 12WS

--- ts_scripts/12WS.R
+++ ts_scripts/12WS.R
@@ -1,20 +1,16 @@
-#script to read in WS data 
-
+#script for WS data analysis
+#libraries and wd----
 library(dplyr)
 library(readxl)
 library(tidyr)
 library(stringr)
 
+setwd('/Users/Oskar/Documents/UMass/IGEA/igea22/')
 
-
-setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
-
-#group12 raw water data
+#group12 raw water data read-in----
 WS_1 = read_xlsx('Raw_water_data/12WS.xlsx')
 
-
-
-#group12 df
+#create group12 control df and linear regression----
 WS1_norm_df=WS_1%>%
   select('Line', 'Analysis', 'Inj Nr', 'd(18_16)Mean', 'd(D_H)Mean', 'Ignore', 'Identifier_1', 'Identifier_2')%>%
   rename("meanO"="d(18_16)Mean")%>% 
@@ -25,15 +21,11 @@ WS1_norm_df=WS_1%>%
   mutate(standardO=case_when(Identifier_1 == "Picarro zero 1 6_23_22" | Identifier_1 == "Picarro zero 2 6_23_22" | Identifier_1 == "Picarro zero 3 6_23_22"~.3,
                              Identifier_1 == "Picarro mid 3 6_23_22" | Identifier_1 == "Picarro mid 2 6_23_22"| Identifier_1 == "Picarro mid 1 6_23_22"~-20.6,
                              Identifier_1 == "Picarro depl 3 6_23_22" | Identifier_1 == "Picarro depl 2 6_23_22"| Identifier_1 == "Picarro depl 1 6_23_22"~-29.6,))
-#beta coefficient and intercept for linear regression
+
+#beta coefficients for linear regression
 lm( standardO ~ meanO, data = WS1_norm_df)
 
-
-
-
-
-
-
+#create group12 normalized df----
 WS1_final_df=WS_1%>%
   select('Line', 'Analysis', 'Inj Nr', 'd(18_16)Mean', 'd(D_H)Mean', 'Ignore', 'Identifier_1', 'Identifier_2')%>%
   rename("meanO"="d(18_16)Mean")%>% 
