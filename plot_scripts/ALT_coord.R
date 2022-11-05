9f4054f6b2ac40bcf63022d80ebc51070f2f7fa7
etboudreau <etboudreau@umass.edu>
2022-11-05 13:52:43 -0400

    emma 11/5

--- plot_scripts/ALT_coord.R
+++ plot_scripts/ALT_coord.R
@@ -4,20 +4,34 @@ library(dplyr)
 library(readxl)
 library(ggplot2)
 
+
+
 #setwd(path to data) 
 setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
 
-#read in file
-g3_elev=readRDS('outputs/ALT_3tp17.rds')
+#read in file-----
+g3_elev=readRDS('outputs/munged_3/ALT_3ep07.rds')
 xs3=substr(g3_elev$UID2,8,8)
 
-g1_elev=readRDS('outputs/ALT_tp17.rds')
+g1_elev=readRDS('outputs/munged_12/ALT_EP07.rds')
 xs1=substr(g1_elev$UID2,8,8)
 
 
-g3_coord=readRDS('outputs/master_tp17.rds')
+g3_coord=readRDS('outputs/munged_3/master_3ep07.rds')
+
+g12_coord=readRDS('outputs/munged_12/master_EP07.rds')
+
+#-----
+
+#xs_g3_df=
 
 
+
+
+
+
+
+#--------------------
 
 # p2=ggplot(ep7_elev)+
 #   geom_point(aes(y=ALT, x=UID2, colour="blue"))+ #as.factor makes discreet colors for the points
@@ -37,4 +51,8 @@ g3_coord=readRDS('outputs/master_tp17.rds')
 # 
 # plot(p2)
 
-#--------------------
+
+
+
+
+
---
+++ ts_scripts/read_raw_ts_data_3.R
@@ -0,0 +1,143 @@
+# script to read in basic data and look at it
+library(dplyr)
+library(readxl)
+library(tidyr)
+library(stringr)
+
+
+# setwd (path to the data)
+setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
+
+
+g3_files=list.files('raw_ts_data/group3.n')
+#input_file="3ep07_ts1.txt"
+
+read_function = function(input_file) {
+  
+  reach_ID = strsplit(input_file, "_")[[1]][1]
+
+# read in data ----------
+
+
+#read in total station excel
+
+ts1_excel = read_xlsx('raw_ts_data/fd/fd3_ts1.xlsx')%>%
+  mutate(TS_code  = "1")
+
+
+ts2_excel = read_xlsx('raw_ts_data/fd/fd3_ts2.xlsx')%>%
+  mutate(TS_code  = "2")
+  
+  
+
+
+
+#read in metadata
+metadata_excel = read_xlsx('raw_ts_data/fd/fd3_metadata.xlsx')
+
+
+#read in reach code
+ts1_reach = read.delim(paste0('raw_ts_data/group3.n/',reach_ID,'_ts1.txt'),
+                       header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
+  transmute(Reach = V1)
+ 
+
+
+ts2_reach = read.delim(paste0('raw_ts_data/group3.n/',reach_ID,'_ts2.txt'),
+                         header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
+  transmute(Reach = V1)
+
+
+
+#read in total station txt files
+ts1_txt = read.delim(paste0('raw_ts_data/group3.n/',reach_ID,'_ts1.txt'),
+                      skip = 1, header = TRUE, dec = ".", sep = ',')%>%
+  mutate(TS_code = "1")%>%
+  mutate(Reach=ts1_reach$Reach)%>%
+  # mutate(Reach = gsub('3','', ts1_reach$Reach))%>%
+  mutate(PointID=as.double(PointID))%>%
+  filter(!is.na(PointID))
+# manual intervention to get rid of PT 101 which was being problematic
+
+
+  
+ts2_txt = read.delim(paste0('raw_ts_data/group3.n/',reach_ID,'_ts2.txt'),
+                      skip = 1, header = TRUE, dec = ".", sep = ',')%>%
+  mutate(TS_code = "2")%>%
+  mutate(Reach=ts2_reach$Reach)%>%
+  # mutate(Reach = gsub('3','', ts2_reach$Reach))%>% #trying to remove 3 from reach to match reach_ID
+  mutate(PointID=as.double(PointID))%>%
+  filter(!is.na(PointID))
+
+
+
+
+#----Join data frames
+
+
+
+
+#row bind excel files for ts1 and ts2
+joined_df1 = rbind(ts1_excel,ts2_excel)%>%
+  
+  filter(Reach==toupper(reach_ID))
+
+#row bind txt files for ts1 and ts2
+joined_df2 = rbind(ts1_txt,ts2_txt)
+
+  #select(joined_df4,-Code) #trying to delete column code
+
+
+#created data frame with excel and text file for specific reach
+
+master_df = left_join(joined_df1, joined_df2, by=c('Reach', 'TS_code', 'PointID'))%>%
+
+  mutate(uniqueID = paste(Reach,TS_code,Location,XSection))%>%
+  mutate(AP = substr(uniqueID,10,10))%>%
+  mutate(LWR = substr(uniqueID,9,9))%>%
+  mutate(number = substr(uniqueID,11,11))%>%
+  mutate(UID2 = paste0(Reach,TS_code,LWR,number,XSection))%>%
+  mutate(Elevation = as.double(str_remove_all(Elevation, ' ')))%>%
+  filter(!PointID==101)
+#joined2_excel_txt_df = master_df[!(master_df$PointID==101),] #removing 101 from excel
+#!means not
+  
+  
+
+# joins active layer df and permafrost layer df and creates a new column with elevation difference
+
+
+
+a_df = select(master_df, UID2, AP, LWR, Elevation)%>%
+  filter(AP =='A')%>% 
+  rename("Active" = "AP")%>% 
+  rename("ElevationA" = "Elevation")
+
+
+
+p_df = select(master_df, UID2, AP, LWR, Elevation)%>%
+  filter(AP =='P')%>% 
+  rename("Permafrost" = "AP")%>% 
+  rename("ElevationP" = "Elevation")
+
+
+
+#joins active layer df and permafrost df and creates a new column with elevation difference
+alt_df = left_join(a_df, p_df, by=c('UID2','LWR'))%>%
+  mutate(ALT = (ElevationA-ElevationP))
+
+
+
+#saving master_df and alt_df as rds and adding 3 in front of reach_ID to distinguish group 3 from 1 and 2
+saveRDS(master_df, paste0('outputs/munged_3/master_',reach_ID,'.rds'))
+saveRDS(alt_df, paste0('outputs/munged_3/ALT_',reach_ID,'.rds'))
+
+
+
+
+}
+#lapply takes thing to be looped over in first position and the function in second position
+ lapply(g3_files,read_function)
+ #read_function(g3_files[1])
+
+
