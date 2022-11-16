58f834a7de42f494f721cce88468b12e61f0432c
naterd0g <nathanielwri@umass.edu>
2022-11-16 17:22:36 -0500

    code is better

--- ts_scripts/edit_12_ep.R
+++ ts_scripts/edit_12_ep.R
@@ -8,55 +8,83 @@ library(stringi)
 setwd('C:/Users/ncw02/Downloads/IGEA/')
 
 
-g12_files = list.files('outputs/munged_12/TPs/')
-input_file = "ALT_TP06.rds"
+g12_files = list.files('outputs/munged_12/EPs')
+#input_file = "ALT_EP12.rds"
 
-#edit_function = function(input_file){
+edit_function = function(input_file){
 
 reach_ID = substring(toupper(strsplit(input_file, "_")[[1]][2]), 1, 4)
 
-master = readRDS(paste0('outputs/munged_12/TPs/master_',reach_ID,'.rds'))
-alt_df = readRDS(paste0('outputs/munged_12/TPs/ALT_',reach_ID,'.rds'))
-
+master = readRDS(paste0('outputs/munged_12/EPs/master_',reach_ID,'.rds'))
+alt_df = readRDS(paste0('outputs/munged_12/EPs/ALT_',reach_ID,'.rds'))
 
 # edit the data to exclude all submerged water points----
-
-bank_points = alt_df%>%
-  filter(LRW == 'W')%>%
-  filter(Cross.section != '2')%>%
-  filter(Cross.section != '5')%>%
-  filter(Number != '2')%>%
-  mutate(Xlabel = as.double('0'))
-
 tundra_points = alt_df%>%
   filter(LRW != 'W')%>%
   mutate(Xlabel = Number)
 
+water_points = alt_df%>%
+  filter(LRW == 'W')
+  
+temp = water_points%>%
+  filter(Cross.section !='3')%>%
+  filter(Cross.section !='8')%>%
+  filter(Number != '2')
+
+temp2 = water_points%>%
+  filter(Cross.section == '3' | Cross.section =='8')%>%
+  filter(Number != '2')%>%
+  filter(Number != '3')%>%
+  filter(Number != '4')
+  
+bank_points = rbind(temp, temp2)%>%
+  mutate(Xlabel = as.double('0'))
+
 land_points = rbind(bank_points, tundra_points)
+
 
 # ----
 
 
 # edit the data to only have submerged water points----
 
-
-
-submerged_points = anti_join(alt_df, land_points, by = 'UID2')%>%
+submerged_points = anti_join(water_points, bank_points, by = 'UID2')%>%
   mutate(Xlabel = 'S')
 
-
+# step11 = alt_df%>%
+#   filter(LRW == 'W')%>%
+#   mutate(Temp = paste0(LRW, Number))%>%
+#   filter(Temp != 'W1')%>%
+#   filter(Temp != 'W5')
 # 
-ALT_violin = rbind(land_points, submerged_points)#%>%
-  #filter(UID2 != 'EP7W381')
+# x3_water = step11%>%
+#   filter(Cross.section =='3')%>%
+#   filter(Temp!='W2')%>%
+#   filter(Temp!='W4')
+# 
+# x8_water = step11%>%
+#   filter(Cross.section =='8')%>%
+#   filter(Temp!='W2')%>%
+#   filter(Temp!='W4')
+# 
+# most_water = step11%>%
+#   filter(Temp!='W3')
+# 
+# submerged_points = rbind(most_water, x3_water, x8_water)%>%
+#   mutate(Xlabel = 'S')
+
+
+# create the final df by joining land points and submerged points
+ALT_violin = rbind(land_points, submerged_points)
 
 saveRDS(ALT_violin, paste0('outputs/munged_12/ALT_violin_',reach_ID,'.rds'))
 
 # ----
 
 
-#}
+}
 
-#dummy = lapply(g12_files,edit_function)
+dummy = lapply(g12_files,edit_function)
 
 
 
--- ts_scripts/edit_12_tp.R
+++ ts_scripts/edit_12_tp.R
@@ -8,7 +8,7 @@ library(stringi)
 setwd('C:/Users/ncw02/Downloads/IGEA/')
 
 
-g12_files = list.files('outputs/munged_12/TPs')
+g12_files = list.files('outputs/munged_12/TPs/')
 input_file = "ALT_TP06.rds"
 
 #edit_function = function(input_file){
@@ -18,66 +18,33 @@ reach_ID = substring(toupper(strsplit(input_file, "_")[[1]][2]), 1, 4)
 master = readRDS(paste0('outputs/munged_12/TPs/master_',reach_ID,'.rds'))
 alt_df = readRDS(paste0('outputs/munged_12/TPs/ALT_',reach_ID,'.rds'))
 
+
 # edit the data to exclude all submerged water points----
 
-step1 = alt_df%>%
-  filter(LRW != 'W')%>%
-  mutate(Temp = paste0(LRW, Number))%>%
-  filter(Temp != 'W2')%>%
-  filter(Temp != 'W4')%>%
+bank_points = alt_df%>%
+  filter(LRW == 'W')%>%
+  filter(Cross.section != '2')%>%
+  filter(Cross.section != '5')%>%
+  filter(Number != '2')%>%
   mutate(Xlabel = as.double('0'))
 
-step2 = step1%>%
-  filter(Cross.section !='3')%>%
-  filter(Cross.section !='8')
-
-step3 = step1%>%
-  filter(Cross.section == '3' | Cross.section == '8')%>%
-  filter(Number != '3')%>%
-  mutate(Xlabel = '0')
-
-step4 = alt_df%>%
+tundra_points = alt_df%>%
   filter(LRW != 'W')%>%
-  mutate(Temp = paste0(LRW, Number))%>%
   mutate(Xlabel = Number)
 
-step5 = rbind(step2, step3)
-
-land_points = rbind(step4, step5)
+land_points = rbind(bank_points, tundra_points)
 
 # ----
 
 
 # edit the data to only have submerged water points----
-
-step11 = alt_df%>%
-  filter(LRW == 'W')%>%
-  mutate(Temp = paste0(LRW, Number))%>%
-  filter(Temp != 'W1')%>%
-  filter(Temp != 'W5')
-
-x3_water = step11%>%
-  filter(Cross.section =='3')%>%
-  filter(Temp!='W2')%>%
-  filter(Temp!='W4')
-
-x8_water = step11%>%
-  filter(Cross.section =='8')%>%
-  filter(Temp!='W2')%>%
-  filter(Temp!='W4')
-
-most_water = step11%>%
-  filter(Temp!='W3')
-
-submerged_points = rbind(most_water, x3_water, x8_water)%>%
+submerged_points = anti_join(alt_df, land_points, by = 'UID2')%>%
   mutate(Xlabel = 'S')
 
+# create the final df by joining land points and submerged points
+ALT_violin = rbind(land_points, submerged_points)
 
-# 
-ultimate = rbind(land_points, submerged_points)#%>%
-  #filter(UID2 != 'EP7W381')
-
-saveRDS(ultimate, paste0('outputs/munged_12/ALT_violin_',reach_ID,'.rds'))
+saveRDS(ALT_violin, paste0('outputs/munged_12/ALT_violin_',reach_ID,'.rds'))
 
 # ----
 
