d8eb197250756d9a4917241ea77a115b6c8706d1
naterd0g <nathanielwri@umass.edu>
2022-11-30 10:32:10 -0500

    test

--- ts_scripts/load_12_tp17.R
+++ ts_scripts/load_12_tp17.R
@@ -8,9 +8,8 @@ library(stringi)
 setwd('C:/Users/ncw02/Downloads/IGEA/')
 
 g12_files = list.files('raw_ts_data/group12.n')
-#input_file = "tp06_ts1.txt"
+input_file = "tp17_ts2.txt"
 
-read_function = function(input_file){
 
 reach_ID = strsplit(input_file, "_")[[1]][1]
   
@@ -33,25 +32,11 @@ names(ts2_excel) = make.names(names(ts2_excel), unique = TRUE)
 metadata_excel = read_xlsx('raw_ts_data/fd/fd12_metadata.xlsx')%>%
   rename("Sample.elevation" = "Elevation")
 
-# extract reach from txt file for ts1 (should be identical for ts2)
-ts1_reach = read.delim(paste0('raw_ts_data/group12.n/',reach_ID,'_ts1.txt'),
-                       header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
-  transmute(Reach = V1)
 
 # extract reach from txt file for ts2 (should be identical for ts1)
 ts2_reach = read.delim(paste0('raw_ts_data/group12.n/',reach_ID,'_ts2.txt'),
                         header = FALSE, nrows= 1, dec = ".", sep = '\t')%>%
   transmute(Reach = V1)
-
-# read in ts1 xyz
-ts1_txt = read.delim(paste0('raw_ts_data/group12.n/',reach_ID,'_ts1.txt'),
-                      skip = 1, header = TRUE, dec = ".", sep = ',')%>%
-  mutate(TS_code = "1")%>%
-  mutate(Reach = ts1_reach$Reach)%>%
-  mutate(PointID = as.double(PointID))
-
-# manual intervention to get rid of PT 101 which was being problematic
-ts1_txt = ts1_txt[2:nrow(ts1_txt),]
 
 # read in ts2 xyz
 ts2_txt = read.delim(paste0('raw_ts_data/group12.n/',reach_ID,'_ts2.txt'),
@@ -70,7 +55,6 @@ ts2_txt = ts2_txt[2:nrow(ts2_txt),]
 
 # join data frames ----
 reach_ID = toupper(reach_ID)
-print(reach_ID)
 
 # this joins excel (digitized data) with metadata for ts1
 joined_df1 = left_join(ts1_excel, metadata_excel, by='Reach')%>%
@@ -81,12 +65,11 @@ joined_df2 = left_join(ts2_excel, metadata_excel, by='Reach')%>%
   filter(Reach == reach_ID)
 
 # this joins the previous file with txt file data 
-joined_df3 = left_join(joined_df1, ts1_txt, by=c('Reach','PointID','TS_code'))
 joined_df4 = left_join(joined_df2, ts2_txt, by=c('Reach','PointID','TS_code'))
 
 # this joins both ts1 and ts2 data to complete the entire reach
 # creates a unique ID and another unique ID without A's and P's
-master_df = rbind(joined_df3, joined_df4)%>%
+master_df = joined_df4%>%
   mutate(uniqueID = paste0(Reach, PointID, Location, Cross.section, TS_code))%>%
   mutate(LRW = substr(uniqueID,8,8))%>%
   mutate(Type = substr(uniqueID,9,9))%>%
@@ -122,15 +105,9 @@ print(input_file)
 print((nrow(alt_df) - length(unique(alt_df$UID2)))/nrow(alt_df))
 
 
-saveRDS(master_df, paste0('outputs/munged_12/master_',reach_ID,'.rds'))
-saveRDS(alt_df, paste0('outputs/munged_12/ALT_',reach_ID,'.rds'))
+saveRDS(master_df, paste0('outputs/munged_12/TPs/master_',reach_ID,'.rds'))
+saveRDS(alt_df, paste0('outputs/munged_12/TPs/ALT_',reach_ID,'.rds'))
 
-
-
-}
-
-
-lapply(g12_files,read_function)
 
 # --------
 
