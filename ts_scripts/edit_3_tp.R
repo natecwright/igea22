05d20e819b7034c03b1b8eb88d949429abe9b636
etboudreau <etboudreau@umass.edu>
2022-11-30 16:48:34 -0500

    emma function not working

--- ts_scripts/edit_3_tp.R
+++ ts_scripts/edit_3_tp.R
@@ -8,28 +8,32 @@ library(stringi)
 setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
 
 
-g3_files = list.files('outputs/munged_3/')
+g3_files = list.files('outputs/munged_3/TPs/')
 #input_file = "ALT_TP06.rds"
 
 edit_function = function(input_file){
   
   reach_ID = substring(toupper(strsplit(input_file, "_")[[1]][2]), 1, 4)
   
-  master = readRDS(paste0('outputs/munged_3/TPs/master_',reach_ID,'.rds'))
-  alt_df = readRDS(paste0('outputs/munged_3/TPs/ALT_',reach_ID,'.rds'))
+  master = readRDS(paste0('outputs/munged_3/TPs/master_3',reach_ID,'.rds'))
+  alt_df = readRDS(paste0('outputs/munged_3/TPs/ALT_3',reach_ID,'.rds'))
   
   
   # edit the data to exclude all submerged water points----
   
   tundra_points = alt_df%>%
-    filter(LRW != 'W')%>%
-    mutate(Xlabel = Number)
+    mutate(number= substr(UID2,8,8))%>%
+    mutate(XSection= substr(UID2,9,9))%>%
+    filter(LWR!= 'W')%>%
+    mutate(Xlabel = number)
   
   bank_points = alt_df%>%
-    filter(LRW =='W')%>%
+    mutate(number= substr(UID2,8,8))%>%
+    mutate(XSection= substr(UID2,9,9))%>%
+    filter(LWR =='W')%>%
     filter(XSection != '2')%>%
     filter(XSection != '5')%>%
-    filter(Number != '2')%>%
+    filter(number != '2')%>%
     mutate(Xlabel = as.double('0'))
   
   land_points = rbind(tundra_points, bank_points)
@@ -38,8 +42,11 @@ edit_function = function(input_file){
   
   
   # edit the data to only have submerged water points----
-  submerged_points = anti_join(alt_df, land_points)%>%
-    mutate(Xlabel = 'S')
+  submerged_points = anti_join(alt_df, land_points) %>%
+    mutate(number= substr(UID2,8,8)) %>%
+    mutate(XSection= substr(UID2,9,9)) %>%
+    mutate(Xlabel = 'S') 
+   
   
   
   #create final df
