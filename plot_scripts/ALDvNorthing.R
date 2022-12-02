02a354b75d6984da3b929746fa505577a46184a7
etboudreau <etboudreau@umass.edu>
2022-12-02 13:45:20 -0500

    Emma 12/2

---
+++ plot_scripts/ALDvNorthing.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(readxl)
+library(ggplot2)
+
+
+
+
+setwd('/Users/emmaboudreau/Documents/GitHub/igea22/')
+
+
+g3_files = list.files('outputs/munged_3/')
+#input_file = "ALT_3EP01.rds"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
