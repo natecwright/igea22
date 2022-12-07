daec35fdf9dfd6ed073eb3da5587380f70445f2f
StellaWenczel <wenczel@umass.edu>
2022-12-07 11:02:20 -0500

    Update T-test.R

--- plot_scripts/T-test.R
+++ plot_scripts/T-test.R
@@ -11,17 +11,18 @@ setwd('/Users/Stella/OneDrive - University of Massachusetts/Documents/IGEA/Munge
 
 
 #read in file
-Sub=readRDS('ep_early.rds')%>%
-  filter(Xlabel==1 | Xlabel==2 | Xlabel==3 | Xlabel==4) #data set 1 to be compared in t test
+Sub=readRDS('ep_early.rds')#%>%
+  #filter(Xlabel==1 | Xlabel==2 | Xlabel==3 | Xlabel==4) #data set 1 to be compared in t test
   #filter(Xlabel==1)
+var(Sub$ALT)
 
+Notsub=readRDS('tp_early.rds')#%>%
+  #filter(Xlabel=='S') #data set 2 to be compared
+var(Notsub$ALT)  
 
-Notsub=readRDS('ep_early.rds')%>%
-  filter(Xlabel=='S') #data set 2 to be compared
-  
-EPe_S_all=t.test(Sub$ALT, Notsub$ALT, var.equal=TRUE) #t test for the alt columns of two sets of data to be compared
+EPe_TPe=t.test(Sub$ALT, Notsub$ALT, var.equal=F) #t test for the alt columns of two sets of data to be compared
 
-
+EPe_TPe_p=EPe_TPe$p.value
 
 
 
